a63b03c742a98dbaca297ff99ffc559994cb5b2a
Mauricio Hidalgo <mrhidalgo@miners.utep.edu>
2019-11-20 16:43:15 -0700

    Deleted Comment

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'main.ui'
-#
-# Created by: PyQt5 UI code generator 5.13.0
-#
-# WARNING! All changes made in this file will be lost!
 
 import os, sys, r2pipe, json
 import pymongo
