423f82914bb0f172e74e5726bdc0b2a3e62c9c1b
Tavo <gortiz8@miners.utep.edu>
2019-12-01 20:37:30 -0700

    POI Updating

--- PointOfInterest.py
+++ PointOfInterest.py
@@ -2,7 +2,6 @@ class POI:
 
     # Initializer / Instance Attributes
     def __init__(self, name,typeP, output):
-        self.typeP = None
+        self.typeP = typeP
         self.name = name
-        self.type = type
         self.out = output
