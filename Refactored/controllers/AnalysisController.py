4dd3cb5476ea59cbd7bdbfeaccf113753d995562
Mauricio Hidalgo <mrhidalgo@miners.utep.edu>
2019-12-02 17:50:31 -0700

    Revert "Revert "Revert "Revert "Revert "Merge branch 'mauricio' into development"""""
    This reverts commit b020d1129ef1eb15be2f0b16a85c8635c839f632.

--- Refactored/controllers/AnalysisController.py
+++
@@ -1,9 +0,0 @@
-class AnalysisController(object):
-
-    def __init__(self, analysis_tab):
-        self.analysis_tab = analysis_tab
-        #self.analysis_tab.projectNewButton.clicked.connect(self.projectWindow)
-
-    #def projectWindow(self):
-        #self.project_tab.setupUiCreateProject(self.project_tab.windowNew)
-        #self.project_tab.windowNew.show()
