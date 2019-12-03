9689b2ca1e755aa03c46e2e2590eb0540537817e
Mauricio Hidalgo <mrhidalgo@miners.utep.edu>
2019-12-02 17:50:31 -0700

    Revert "Merge branch 'mauricio' into development"
    This reverts commit 729bb3475ed96353957445c4460bba237738c238.

--- Refactored/controllers/POIController.py
+++
@@ -1,9 +0,0 @@
-class POIController(object):
-
-    def __init__(self, poi_tab):
-        self.poi_tab = poi_tab
-        #self.analysis_tab.projectNewButton.clicked.connect(self.projectWindow)
-
-    #def projectWindow(self):
-        #self.project_tab.setupUiCreateProject(self.project_tab.windowNew)
-        #self.project_tab.windowNew.show()
